f70ee309aea7e04af1e7f7f35a21ca97126f6f4e
daveokpare <okparedave@gmail.com>
2025-09-04 09:31:38 +0100

    feat: implement OpenAI integration and response generation

--- open-deep-research/main.py
+++ open-deep-research/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from open-deep-research!")
+import os
+from openai import OpenAI
+from arize.otel import register
+from dotenv import load_dotenv
+from openinference.instrumentation.openai import OpenAIInstrumentor
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+tracer_provider = register(
+    space_id = os.getenv("ARIZE_SPACE_ID"), # in app space settings page
+    api_key = os.getenv("ARIZE_API_KEY"), # in app space settings page
+    project_name = "open-deep-research", 
+)
+
+OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
+
+client = OpenAI()
+
+response = client.responses.create(
+  model="gpt-4.1",
+  input="Tell me a three sentence bedtime story about a unicorn."
+)
+
+print(response)
